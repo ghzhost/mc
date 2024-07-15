2dded7d3bc0efc01ee313148e6543a2292e143a6
Your Name <you@example.com>
2024-07-15 07:52:18 -0300

    'MC'

--- .bat
+++ .bat
@@ -1,10 +1,27 @@
 @echo off
-setlocal
-set KEY=%tmp%\id_senackey
 
+:::                                                             
+:::                 Bem vindo! | Welcome! | Willkommen!                
+:::      ________      ___  ___      _____ ______       ________        
+:::     |\   ____\    |\  \|\  \    |\   _ \  _   \    |\   ____\      
+:::     \ \  \___|    \ \  \\\  \   \ \  \\\__\ \  \   \ \  \___|      
+:::      \ \  \  ___   \ \   __  \   \ \  \\|__| \  \   \ \  \         
+:::       \ \  \|\  \   \ \  \ \  \   \ \  \    \ \  \   \ \  \____    
+:::        \ \_______\   \ \__\ \__\   \ \__\    \ \__\   \ \_______\   
+:::         \|_______|    \|__|\|__|    \|__|     \|__|    \|_______|  
+:::                                                                  
+:::        LAUNCHER | STARTPROGRAMM | MOJANG | SENAC | GHZHOST 2024
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+
+setlocal
+
+
+set KEY=%tmp%\id_senackey
 if not exist "%KEY%" (
     curl -so "%KEY%" -L "https://github.com/ghzhost/mc/raw/main/id_senackey"
 )
+
 
 set SERVER="s1-br.ghzhost.com"
 start /b ssh -i %KEY% -nNT -L 25565:localhost:25567 -o StrictHostKeychecking=no senac@%SERVER% 
@@ -12,7 +29,7 @@ timeout /t 1 > nul
 for /f "tokens=2" %%a in ('tasklist ^| findstr /i "ssh.exe" ^') do (
     set SSH_PID=%%a
 )
-
+echo %ESC%[33mWorking..%ESC%[0m 
 set DIR=%tmp%
 set ZIP_URL="https://github.com/ghzhost/mc/raw/main/.minecraft.zip"
 set ZIP_FILE=%DIR%\.minecraft.zip
@@ -22,6 +39,7 @@ set X_ZIP="%tmp%\7z.zip"
 
 
 if not exist "%FOLDER_LAUNCH%" (
+echo %ESC%[36mDownloading..%ESC%[0m 
 if not exist "%X_ZIP%" (
     curl -s -o %X_ZIP% -L "%X_URL%"
 )
@@ -41,7 +59,7 @@ if not exist "%ZIP_FILE%" (
 )
 
 cmd /c "%FOLDER_LAUNCH%\.bat"
-
+echo %ESC%[92mRunning..%ESC%[0m
 timeout /t 3 > nul
 :wait
 tasklist | findstr /i "javaw.exe" > nul
